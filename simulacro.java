daac25dbef5238ad2892a8c2368ffc8594ce8b93
blackback <miguelacamara2600@gmail.com>
2024-10-25 09:02:40 +0200

    Simulacro

---
+++ simulacro.java
@@ -0,0 +1,22 @@
+public class simulacro {
+    public static void main(String[] args) {
+        int numero1 = 8;
+        int numero2 = 5;
+
+        if (numero1 > numero2) {
+            System.out.println("El mayor es: " + numero1);
+            System.out.println("Los números son diferentes.");
+        } else if (numero2 > numero1) {
+            System.out.println("El mayor es: " + numero2);
+            System.out.println("Los números son diferentes.");
+        } else {
+            System.out.println("Ambos números son iguales.");
+        }
+    }
+}
+
+
+    
+    }
+
+}
